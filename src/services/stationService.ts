3ecb62a305cb69e1539308e245174f795cf0fee4
Loïck Postel <postel.loick@gmail.com>
2025-12-12 10:52:53 +0100

    station service and hook

---
+++ src/services/stationService.ts
@@ -0,0 +1,20 @@
+import axiosInstance from "@/lib/axios"
+import type { Station, StationForm } from "@/validations/station"
+
+export const stationService = {
+    getAll: async (): Promise<Station[]> => {
+        const response = await axiosInstance.get('stations')
+        return response.data
+    },
+    create: async (station: StationForm): Promise<Station> => {
+        const response = await axiosInstance.post('stations', station)
+        return response.data
+    },
+    update: async (id: number, station: StationForm): Promise<Station> => {
+        const response = await axiosInstance.patch(`stations/${id}`, station)
+        return response.data
+    },
+    delete: async (id: number): Promise<void> => {
+        await axiosInstance.delete(`stations/${id}`)
+    }
+}
